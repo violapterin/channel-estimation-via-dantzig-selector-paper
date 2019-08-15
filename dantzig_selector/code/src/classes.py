Code: Correct normalizing constants

dantzig_selector/code/src/classes.py
@@ -4,63 +4,6 @@ import cvxpy as cp
 import constants as cst
 import functions as fct
 
-class Hh:
-    def __init__ (self):
-        self.val = np.zeros ((cst.nn_hh, cst.nn_hh), dtype=complex)
-    
-    def gen (self):
-        for l in range (cst.ll_path):
-            aG_l = np.random.normal (cst.amp_mean_hh, cst.amp_std_hh)
-            pG_l = (cst.dist_antenna /cst.lG) * np.sin (np.random.uniform (0, 2 * np.pi))
-            tG_l = (cst.dist_antenna /cst.lG) * np.sin (np.random.uniform (0, 2 * np.pi))
-            self.val += aG_l * (fct.arr_resp (pG_l)) @ fct.arr_resp (pG_l).conj().T
-
-class Zz:
-    def __init__ (self):
-        self.val = np.zeros ((cst.nn_yy, cst.nn_yy), dtype=complex)
-    
-    def gen (self):
-        for i in range (cst.nn_yy):
-            for j in range (cst.nn_yy):
-                self.val [i] [j] = (
-                    np.random.normal (0, 1)
-                    + 1J * np.random.normal (0, 1))
-
-class Ff_bb:
-    def __init__ (self):
-        self.val = np.zeros ((cst.nn_rr, cst.nn_yy), dtype=complex)
-
-    def gen (self):
-        for i in range (cst.nn_rr):
-            for j in range (cst.nn_yy):
-                self.val [i] [j] = (
-                    np.random.normal (cst.part_mean_bb, cst.part_std_bb)
-                    + 1J * np.random.normal (cst.part_mean_bb, cst.part_std_bb))
-        self.val /= np.sqrt (cst.nn_yy)
-
-class Ff_rr:
-    def __init__ (self):
-        self.val = np.zeros ((cst.nn_hh, cst.nn_rr), dtype=complex)
-
-    def gen (self):
-        for i in range (cst.nn_hh):
-            for j in range (cst.nn_rr):
-                idx_phase = np.random.randint (cst.num_grid_phase)
-                self.val[i][j] = np.exp (2 * np.pi * 1J * idx_phase /cst.num_grid_phase)
-        self.val /= np.sqrt (cst.nn_rr)
-        
-class Ww_bb:
-    def __init__ (self):
-        self.val = np.zeros ((cst.nn_yy, cst.nn_rr), dtype=complex)
-
-    def gen (self):
-        for i in range (cst.nn_yy):
-            for j in range (cst.nn_rr):
-                self.val [i] [j] = (
-                        np.random.normal (cst.part_mean_bb, cst.part_std_bb)
-                        + 1J * np.random.normal (cst.part_mean_bb, cst.part_std_bb))
-        self.val /= np.sqrt (cst.nn_yy)
-
 
 class Dd_ss:
     def __init__(self, pp, y, gG):
@@ -75,30 +18,37 @@ class Dd_ss:
         c=[]
         d=[]
         for i in range (cst.nn_h):
-            aa.append (np.concatenate ([fct.indication_repr_mat (i),
-                                       np.zeros ((2 * cst.nn_h, cst.nn_h))],
-                                       axis= 1))
+            aa.append (
+                np.concatenate (
+                    [fct.indication_repr_mat (i),
+                        np.zeros ((2 * cst.nn_h, cst.nn_h))],
+                    axis= 1))
             b.append (np.zeros ((2 * cst.nn_h)))
-            c.append (np.concatenate ([np.zeros ((2 * cst.nn_h)), fct.indication_vec (i)]))
+            c.append (
+                np.concatenate (
+                    [np.zeros ((2 * cst.nn_h)), fct.indication_vec (i)]))
             d.append (0)
-        for i in range (cst.nn_h, 2 * cst.nn_h):
-            aa.append (np.concatenate ([-fct.indication_repr_mat (i -cst.nn_h)
-                                        @ fct.find_repr_mat (self.pp.conj().T)
-                                        @ fct.find_repr_mat (self.pp),
-                                        np.zeros ((2 * cst.nn_h, cst.nn_h))],
-                       axis= 1))
-            b.append (fct.indication_repr_mat (i -cst.nn_h)
-                      @ fct.find_repr_mat (self.pp.conj().T)
-                      @ fct.find_repr_vec (self.y))
+        for i in range (cst.nn_h):
+            aa.append (
+                np.concatenate (
+                    [-fct.indication_repr_mat (i)
+                            @ fct.find_repr_mat (self.pp.conj().T)
+                            @ fct.find_repr_mat (self.pp),
+                        np.zeros ((2 * cst.nn_h, cst.nn_h))],
+                    axis= 1))
+            b.append (fct.indication_repr_mat (i)
+                @ fct.find_repr_mat (self.pp.conj().T)
+                @ fct.find_repr_vec (self.y))
             c.append (np.zeros ((3 * cst.nn_h)))
             d.append (self.gG)
-        t = np.concatenate ([np.zeros ((2 * cst.nn_h)), np.ones ((cst.nn_h))])
+        t = np.concatenate (
+            [np.zeros ((2 * cst.nn_h)),
+                np.ones ((cst.nn_h))])
         x = cp.Variable (3 * cst.nn_h)
 
         constr = [cp.SOC (c[i].T @ x + d[i], aa[i] @ x + b[i]) for i in range (2 * cst.nn_h)]
         prob = cp.Problem (cp.Minimize (t.T@x), constr)
         prob.solve ()
-        #print (x.value) #XXX
 
         x_hat = x.value
         g_repr_hat = x_hat [0 : 2 * cst.nn_h]

dantzig_selector/code/src/constants.py
@@ -1,22 +1,65 @@
 import numpy as np
 
-dist_antenna = 2
-ll_path = 8
+# # Part 1
 
-lG = 3
+# precoder dimension
 nn_yy = 3
-nn_rr = 5
-nn_hh = 7
+nn_rr = 6
+nn_hh = 9
 nn_y = nn_yy *nn_yy
 nn_h = nn_hh *nn_hh
+num_grid_phase = 16
 
-num_grid_phase = 8
-amp_mean_hh = 3
-amp_std_hh = 1/np.sqrt(2)
-part_mean_bb = 1
-part_std_bb = 0.2
+# channel parameters
+lG_ant = 3
+d_ant = 2
+ll = 4
 
-#power_constr
+# simulation count
+num_scale_ssnnrr = 6
+num_try_gG = 2
+num_repeat = 8
 
+# # # # # # # # # # # # # # # #
+# # Part 2
+# # Uncomment the following to try different values:
 
+# # precoder dimension
+# nn_yy = 4
+# nn_rr = 8
+# nn_hh = 12
+# nn_y = nn_yy *nn_yy
+# nn_h = nn_hh *nn_hh
+# num_grid_phase = 16
 
+# # channel parameters
+# lG_ant = 3
+# d_ant = 2
+# ll = 4
+
+# # simulation count
+# num_scale_ssnnrr = 8
+# num_try_gG = 3
+# num_repeat = 12
+
+# # # # # # # # # # # # # # # #
+# # Part 3
+# # Uncomment the following to try different values:
+
+# # precoder dimension
+# nn_yy = 8
+# nn_rr = 12
+# nn_hh = 16
+# nn_y = nn_yy *nn_yy
+# nn_h = nn_hh *nn_hh
+# num_grid_phase = 16
+
+# # channel parameters
+# lG_ant = 3
+# d_ant = 2
+# ll = 4
+
+# # simulation count
+# num_scale_ssnnrr = 12
+# num_try_gG = 4
+# num_repeat = 16

dantzig_selector/code/src/functions.py
@@ -6,15 +6,8 @@ import constants as cst
 import classes as cls
 
 def mat_complex_normal (nn_1, nn_2):
-    return ((np.random.normal (
-        cst.part_mean_bb,
-        cst.part_std_bb,
-        (nn_1, nn_2))
-    +np.random.normal (
-        cst.part_mean_bb,
-        cst.part_std_bb,
-        (nn_1, nn_2))))
-
+    return ((np.random.normal (0, 1, (nn_1, nn_2))
+        +1J * np.random.normal (0, 1, (nn_1, nn_2))))
 
 def mat_uniform_phase (nn_1, nn_2):
     return (
@@ -45,25 +38,29 @@ def ww_rr ():
 
 def hh():
     ret =np.zeros ((cst.nn_hh, cst.nn_hh), dtype=complex)
-    for l in range (cst.ll_path):
-        aG_l = np.random.normal (cst.amp_mean_hh, cst.amp_std_hh)
-        pG_l = (cst.dist_antenna /cst.lG) * np.sin (np.random.uniform (0, 2 * np.pi))
-        tG_l = (cst.dist_antenna /cst.lG) * np.sin (np.random.uniform (0, 2 * np.pi))
-        ret += aG_l * (arr_resp (pG_l)) @ arr_resp (pG_l).conj().T
+    for _ in range (cst.ll):
+        aG = (np.random.normal (0, 1)
+            + 1J * np.random.normal (0, 1))
+        pG = (2 * np.pi * (cst.d_ant /cst.lG_ant)
+            * np.sin (np.random.uniform (0, 2 * np.pi)))
+        tG = (2 * np.pi * (cst.d_ant /cst.lG_ant)
+            * np.sin (np.random.uniform (0, 2 * np.pi)))
+        ret += aG * arr_resp (pG) @ arr_resp (tG).conj().T
     return ret
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-def arr_resp (pG):
-    return np.array ([np.exp (1J *i *pG) for i in range (cst.nn_hh)])
-
 def kk (): # DFT matrix
     ret =np.zeros ((cst.nn_hh, cst.nn_hh), dtype=complex)
     for i in range (cst.nn_hh):
         for j in range (cst.nn_hh):
-            ret [i] [j] =(1 /np.sqrt (cst.nn_h)) *np.exp (2 *np.pi *1J *i *j /cst.nn_hh)
+            ret [i] [j] =(1 /np.sqrt (cst.nn_hh)) *np.exp (2 *np.pi *1J *i *j /cst.nn_hh)
     return ret
+
+def arr_resp (t):
+    return ((1 / np.sqrt (cst.nn_hh))
+        * np.array ([np.exp (1J * i * t) for i in range (cst.nn_hh)]))
 
 def find_repr_vec (v):
     ret =np.zeros ((2*len (v)))
@@ -109,7 +106,7 @@ def indication_repr_mat (n_h):
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-def draw (dat_x, list_dat, label_x, label_y, title):
+def draw (dat_x, list_dat_y, label_x, label_y, title):
     plt.close ("all")
     fig =plt.figure ()
     plt.title (title, fontsize = 15)
@@ -129,21 +126,22 @@ def draw (dat_x, list_dat, label_x, label_y, title):
     width_line = 3 # fixed line width
 
     size_line =len (dat_x)
-    for i in range (len (list_dat)):
-        dat =list_dat[i]
-        assert (size_line == len (dat.arr))
-        plt.plot (dat_x,
-                  dat.arr,
-                  markersize =size_marker,
-                  linewidth =width_line,
-                  linestyle =list_style [int (i % num_style)],
-                  color =list_color [int (i % num_color)],
-                  marker =list_marker [int (i % num_marker)],
-                  )
+    for i in range (len (list_dat_y)):
+        dat_y =list_dat_y [i]
+        assert (size_line == len (dat_y))
+        plt.plot (
+            dat_x,
+            dat_y,
+            markersize =size_marker,
+            linewidth =width_line,
+            linestyle =list_style [int (i % num_style)],
+            color =list_color [int (i % num_color)],
+            marker =list_marker [int (i % num_marker)])
     # TODO: legend
 
-    path_fig_out =(os.path.abspath (os.path.join (os.getcwd (), os.path.pardir))
-                  +"/plt/" +title +".png")
+    path_fig_out =(
+        os.path.abspath (os.path.join (os.getcwd (), os.path.pardir))
+        +"/plt/" +title +".png")
     if os.path.isfile(path_fig_out):
         os.system("rm "+path_fig_out)
     plt.savefig (path_fig_out, bbox_inches ="tight")

dantzig_selector/code/src/main_1.py
@@ -1,41 +1,74 @@
+#! /usr/bin/env python3
+
 import numpy as np
 import cvxpy as cp
 import matplotlib.pyplot as plt
+import time
 
 import constants as cst
 import classes as cls
 import functions as fct
 
-list_ss_nn_rr = [0.01 * 1.5 ** x for x in range (0,5)]
+
+# # # # # # # # # # # # # # # # 
+
+pow_low_gG = -int (cst.num_try_gG /2)
+pow_high_gG = cst.num_try_gG + pow_low_gG
+list_gG = [
+    np.sqrt (np.log (cst.nn_h)) * np.sqrt(2) ** x
+    for x in reversed (range (int(pow_high_gG), int(pow_low_gG), -1))]
+
+pow_low_ssnnrr = -int (cst.num_scale_ssnnrr /2)
+pow_high_ssnnrr = cst.num_scale_ssnnrr + pow_low_ssnnrr
+list_ssnnrr = [
+    np.sqrt(2) ** x
+    for x in reversed (range (int(pow_high_ssnnrr), int(pow_low_ssnnrr), -1))]
+
 kk =fct.kk()
-#for ss_nn_rr in list_ss_nn_rr:
+time_start = time.time()
 
-pp = np.kron (
-    fct.ff_bb().T @ fct.ff_rr().T @ kk.conj(),
-    fct.ww_bb() @ fct.ww_rr() @ kk)
-hh =fct.hh()
-gg = kk.conj().T @ hh @ kk
-g = fct.vectorize (gg)
-z = fct.vectorize (fct.zz())
+list_arr_e = []
+for gG in list_gG:
+    arr_e = np.array ([])
+    for ssnnrr in list_ssnnrr:
+        e = 0
+        for _ in range (cst.num_repeat):
+            ff_bb =fct.ff_bb()
+            ff_rr =fct.ff_rr()
+            ww_bb =fct.ww_bb()
+            ww_rr =fct.ww_rr()
+            hh =fct.hh()
+            zz =fct.zz()
+            gg = kk.conj().T @ hh @ kk
 
-#y = pp @ g + z
-y = pp @ g #XXX
+            pp = np.kron (
+                ff_bb.T @ ff_rr.T @ kk.conj(),
+                ww_bb @ ww_rr @ kk)
+            g = fct.vectorize (gg)
+            z = fct.vectorize (zz)
+            y = pp @ g + ssnnrr * z
 
-gG = np.sqrt (2*np.log (cst.nn_h))
-dd_ss = cls.Dd_ss (pp, y, gG);
-dd_ss.run()
+            dd_ss = cls.Dd_ss (pp, y, gG);
+            dd_ss.run()
+            hh_hat = (kk
+                @ fct.inv_vectorize (dd_ss.g_hat, cst.nn_hh, cst.nn_hh)
+                @ kk.conj().T)
+            e += (np.linalg.norm (hh_hat -hh, ord='fro')
+                / np.linalg.norm (hh, ord='fro'))
+        e /= cst.num_repeat
+        arr_e =np.append (arr_e, [e])
+    list_arr_e.append(arr_e)
 
-hh_hat = kk @ fct.inv_vectorize (dd_ss.g_hat, cst.nn_hh, cst.nn_hh) @ kk.conj().T
-dd =hh_hat -hh
+time_stop = time.time()
 
-print(np.linalg.norm (hh, ord='fro'))
-print(np.linalg.norm (hh_hat, ord='fro'))
-print(np.linalg.norm (dd, ord='fro'))
+fct.draw (list_ssnnrr, list_arr_e, "Std of Noise", "Relative Error", "DS_Performance")
 
-#print(np.linalg.norm (ff_bb.val, ord=1))
-#print(np.linalg.norm (ff_rr.val, ord=1))
-#print(np.linalg.norm (ww_bb.val, ord=1))
-#print(np.linalg.norm (ww_rr.val, ord=1))
-#print(np.linalg.norm (hh.val, ord=1))
-#print(np.linalg.norm (zz.val, ord=1))
-#quit()
+print (
+    "total time elapsed: ",
+    ((time_stop - time_start)
+        / (60 * cst.num_scale_ssnnrr * cst.num_repeat * cst.num_try_gG)),
+    " (min)\n")
+print (
+    "averaged time elapsed for each data point: ",
+    time_stop - time_start,
+    " (min)")

dantzig_selector/code/src/test.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import matplotlib.pyplot as plt # plotting functions
 import numpy as np
 import constants as cst
@@ -6,10 +8,18 @@ import os # getcwd
 
 import functions as fct
 
-x =np.random.randint (4, size=2)
-y =np.exp (x)
-print (x)
-print (y)
+#a =[x ** 3 for x in range (-(7/2), 4)]
+#print (a)
+
+#dat_x = np.array ([2,4,6,8])
+#dat_y1 =np.zeros (4)
+#dat_y2 =np.zeros (4)
+#for i in range(4):
+#    dat_y1 [i] = 3 * dat_x [i] + 0.1 * np.random.normal (0, 1)
+#    dat_y2 [i] = 5 * dat_x [i] + 0.1 * np.random.normal (0, 1)
+#list_dat_y =[dat_y1, dat_y2]
+#fct.draw(dat_x, list_dat_y, "foo", "bar", "Quux")
+
 
 #tt =np.array (range(3))
 #pp =np. array ([[4,5,6,3], [0,-2,7,4]])