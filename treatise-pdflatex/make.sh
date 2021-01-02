2d319d757123d9a914f10d8dabcd6cbdfa30611e
Raven I. Pilot <violapterin@gmail.com>
2021-01-03 04:11:53 +0800

    Chap 2 compiles

--- treatise-pdflatex/make.sh
+++ treatise-pdflatex/make.sh
@@ -3,10 +3,11 @@
 NAME=main
 
 clear
-#rm ${NAME}.out ${NAME}.log ${NAME}.bcf ${NAME}.aux ${NAME}.run.xml
-pdflatex -halt-on-error ${NAME}.tex
+rm ${NAME}.out ${NAME}.log ${NAME}.bcf ${NAME}.aux ${NAME}.run.xml
+pdflatex -halt-on-error ${NAME}
 biber ${NAME}
-pdflatex -halt-on-error ${NAME}.tex
+pdflatex -halt-on-error ${NAME}
+pdflatex -halt-on-error ${NAME}
 cp -f ${NAME}.pdf ${NAME}_1.pdf
 cp -f ${NAME}.pdf ${NAME}_2.pdf
 
